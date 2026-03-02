854c1393e69ffe27c70ec5ae3dc22c56bdef4eab
PocketPoke <gitea@snowshiba.com>
2026-03-02 13:51:58 -0500

    Add Linux Support

--- Services/Utils/OSUtils.py
+++ Services/Utils/OSUtils.py
@@ -1,6 +1,7 @@
 from .OSAdapters.BaseAdapter import BaseAdapter
 from .OSAdapters.Windows import WindowsUtils
 from .OSAdapters.MacOS import MacOSUtils
+from .OSAdapters.Linux import LinuxUtils
 
 
-OSUtils: BaseAdapter = WindowsUtils if BaseAdapter.isWindows() else MacOSUtils
+OSUtils: BaseAdapter = WindowsUtils if BaseAdapter.isWindows() else LinuxUtils if BaseAdapter.isLinux() else MacOSUtils
